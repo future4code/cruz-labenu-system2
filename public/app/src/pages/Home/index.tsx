47f8a40f34cb20122a3ae00658457d2621164007
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-06-03 14:54:23 -0300

    services api

--- public/app/src/pages/Home/index.tsx
+++ public/app/src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import { Input } from 'components/Input'
 import * as S from './styles'
+import { useHistory } from 'react-router-dom';
 
 export const Home = () => {
+  const history = useHistory()
   return (
     <S.Container>
       <S.Title>Labenu System Home Page</S.Title>
