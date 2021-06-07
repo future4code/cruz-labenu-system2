cf2068a113103a9e0b0861754a8bcbc6964d7bf2
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-06-07 19:43:27 -0300

    refactor: changed some functional components to arrow function to easy read and export

--- public/app/src/App.tsx
+++ public/app/src/App.tsx
@@ -1,16 +1,12 @@
-import Router from "routes/Router"
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from "styles/global"
+import {Router} from 'routes/Router'
+import {ThemeProvider} from 'styled-components'
+import {GlobalStyle} from 'styles/global'
 
-import { theme } from './styles/theme'
+import {theme} from './styles/theme'
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
-  )
-}
-
-export default App
+export const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Router />
+  </ThemeProvider>
+)
--- public/app/src/routes/Router.tsx
+++ public/app/src/routes/Router.tsx
@@ -1,22 +1,18 @@
 import {Home, Class} from '../pages'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import { Layout } from 'components/Layout'
-import { Students } from 'pages/Students'
-import { Teachers } from 'pages/Teachers'
+import {Layout} from 'components/Layout'
+import {Students} from 'pages/Students'
+import {Teachers} from 'pages/Teachers'
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path='/' children={<Home/>} />
-          <Route exact path='/class' children={<Class/>} />
-          <Route exact path='/students' children={<Students/>} />
-          <Route exact path='/teachers' children={<Teachers/>} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
-  )
-}
-
-export default Router
+export const Router = () => (
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path='/' children={<Home />} />
+        <Route exact path='/class' children={<Class />} />
+        <Route exact path='/students' children={<Students />} />
+        <Route exact path='/teachers' children={<Teachers />} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+)
