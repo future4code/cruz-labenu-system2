a8e54987b81352c06c00165e7d9bd99a00177e05
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-06-07 13:50:06 -0300

    refactor: join students/teachers entities in user, and join hobby/skill intro activity entitie

--- src/utils/schemas/user-search-options.ts
+++ src/utils/schemas/user-search-options.ts
@@ -1,25 +1,25 @@
 import {JSONSchemaType} from 'ajv'
 import {ClassSearchOptions} from '../../shared/types/ClassSearchOptions'
 import {queryOptions} from './query-options'
-import {classAnyProps} from './class-any-props'
+import {userAnyProps} from './user-any-props'
 
-const {name, patron, module: modulo} = classAnyProps.properties
+const {name, nickname, email} = userAnyProps.properties
 
-export const classSearchOptions: JSONSchemaType<ClassSearchOptions> = {
+export const userSearchOptions: JSONSchemaType<ClassSearchOptions> = {
   ...queryOptions,
   type: 'object',
   examples: [
     {
       ...queryOptions.examples[0],
-      name: 'https://example.com/search?name=cru',
-      patron: 'https://example.com/search?patron=oswaldo',
-      module: 'https://example.com/search?module=5'
+      name: 'https://example.com/search?name=marcelino',
+      nickname: 'https://example.com/search?nickname=bananinha',
+      email: 'https://example.com/search?email=gmail'
     }
   ],
   properties: {
     ...queryOptions.properties,
     name,
-    patron,
-    module: modulo
+    nickname,
+    email
   }
 }
